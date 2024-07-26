f4a7e6fd6ae5a1e58e5f610e9678c4a368a84e02
Rai-shwith <ashwith96.96@gmail.com>
2024-07-26 16:26:36 +0530

    Extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Purple': 'hsl(259, 100%, 65%)',
+        'Light-red': 'hsl(0, 100%, 67%)',
+        'Off-white': 'hsl(0, 0%, 94%)',
+        'Light-grey': 'hsl(0, 0%, 86%)',
+        'Smokey-grey': 'hsl(0, 1%, 44%)',
+        'Off-black': 'hsl(0, 0%, 8%)'
+      }
+    },
   },
   plugins: [],
 }
